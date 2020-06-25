80b40ad13c0ab43bc914835616a5e2445cdf1039
xz8la8 <174785876@qq.com>
2020-06-25 16:56:57 +0800

    chore: custom theme

--- config/config.js
+++ config/config.js
@@ -27,4 +27,9 @@ export default defineConfig({
   layout: {
     locale: true,
   },
+  theme: {
+    'layout-header-background': '#104560', // 自定义 dark siderMenu 需要修改Antd的theme layout-header-background, menu-dark-submenu-bg, menu-dark-item-active-bg
+    'menu-dark-submenu-bg': '#0B3043',
+    'menu-dark-item-active-bg': '#2a6590'
+  }
 });
